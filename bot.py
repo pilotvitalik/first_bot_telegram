7228f3d68ea27259cbc4f6f2fca36c41aee82f04
vitaliy_kozhushko <itvkip@yandex.ru>
2022-11-15 20:12:05 +0300

    Token add to .env

---
+++ bot.py
@@ -0,0 +1,5 @@
+import requests
+from get_env import token
+
+url = 'https://api.telegram.org/bot'
+print(token)
---
+++ get_env.py
@@ -0,0 +1,3 @@
+from dotenv import dotenv_values
+
+token = dotenv_values('.env')['TOKEN']
